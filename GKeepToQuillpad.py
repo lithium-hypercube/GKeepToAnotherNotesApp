ff77f38ef48a8c617c12f015f34fffcb19d2dd1a
SilentShadow928 <78055461+SilentShadow928@users.noreply.github.com>
2024-08-23 22:07:34 -0700

    Make GKeepToQuillpad.py convert to notes instead of quillpad

--- GKeepToQuillpad.py
+++ GKeepToQuillpad.py
@@ -1,81 +1,69 @@
-import re
 import sys
 import os
-import subprocess
+import ast
+from datetime import datetime
 
+#idk how to do checklists and such, if it encounters one it might crash - needs testing
+#if you really need this please open a github issue of pull request
+#Yet another notes app is referred to as notes for brevity
 
-#OK :
-#-text content of notes
-#-title of notes
-#-pinned status
-
-#TODO
-#-note date (will be reset to today by quillpad at import)
-#-tags
-#-id (now made with alphabetical order)
-
-
-def extractnote(line):
-    #print(line)
-    #extracts if the note is pinned or not
-    isPinned =re.search(r'"isPinned"(.*?),',line[0]).group(0)
-    #print(ispinned)
-    #extracts title and content
-    note = re.search(r'"text(.*)title":"(.*?)"', line[0]).group(0)
-    note = ''.join((isPinned,note))
-    #print(note)
-    return note
 def main():
-    '''
-    export your google keep notes with takeout
-    unzip the downloaded archive
-    move all json to a separate folder
-    from this json only folder run 
-    python3 GKeepToQuillad.py *.json
-    then run the written sed command
-    compress the res/backup.json file in a zip
-    import this zip in Quillpad and hope it works
-    '''
-    files = sys.argv[1:]
-    respath='./res/'
-    if not os.path.exists(respath):
-        os.makedirs(respath)
-    id=1
-    #Quillpad has all notes in one json file. GKeep has one json file per note. 
-    #create a result file to write all GKeep migrated notes. this file MUST be named backup.json to be imported by Quillpad.
-    filename_all= ''.join((respath,'backup.json'))
-    file_all= open(filename_all,'a')
-    readout=open(filename_all,'r')
-    #Quillpad header
-    file_all.write('{"version":17,"notes":[')
-    for filename in files:
-        filein=open(filename,'r')
-        line = filein.readlines()
-        #print(filename)
-        note=extractnote(line)
+	'''
+	export your google keep notes with takeout
+	unzip the downloaded archive
+	cd to the Keep folder
+	download this python script to the folder you are in
+	from this folder run
+	python3 GKeepToNotes.py *.json
+	import ./res/notes_converted.json in notes and hope it works
+	'''
+	files = sys.argv[1:]
+	respath='./res/'
+	if not os.path.exists(respath):
+		os.makedirs(respath)
+	id=1
+	#notes has all notes in one json file. GKeep has one json file per note.
+	filename_all= ''.join((respath,'notes_converted.json'))
+	file_all= open(filename_all,'a')
+	readout=open(filename_all,'r')
+	#notes header
+	file_all.write('{"version":4,"notes":{')
+	for filename in files:
+		print("Converting " + filename)
+		filein=open(filename,'r')
+		line = filein.readlines()
+		line = line[0].replace("\":false,", "\":False,").replace("\":true,", "\":True,").replace("\\/", "/") #change list to python format
 
-        #fileout = open(''.join((respath, filename)),'w')
-        newline = ''.join(('{',note,''.join((',"id":',str(id),'}'))))
-        #print(note)
-        if id==1 :
-            file_all.writelines(newline)
-        else :
-            file_all.writelines(',')
-            file_all.writelines(newline)
-        #file_all.write(''.join((newline,',')))
-        readout.readlines()
-        #fileout.writelines(newline)
-        filein.close()
-        id=id+1
-    file_all.write(']}')
-    file_all.close()
-    #fileout.close()
-  
-    sedcmd = ''.join(("sed -i -e 's/textContent/content/g' ", filename_all))
-    print("run this sed command : \n")
-    #don't edit the file with a text editor, or use vim -b to keep the file as a [noeol] (no end of line) file, to match Quillpad format. not sure if necessary.
-    print(sedcmd)
-    #subprocess.run([sedcmd])
-print(__name__)
+		if line[len(line)-1:] == "\\":
+			line=line[:-1]
+		parsed_data = ast.literal_eval(line)
+
+		if parsed_data['isTrashed']:
+			notestatus=2
+		elif parsed_data['isArchived']:
+			notestatus=1
+		else:
+			notestatus=0
+
+		if parsed_data['isPinned']:
+			notepinned=2
+		else:
+			notepinned=1
+
+		note_line = ''.join(["\"",str(id),"\":{\"type\":0,\"title\":\"", parsed_data['title'].replace("\n", "\\n").replace("\"", "\\\""), "\",\"content\":\"", parsed_data['textContent'].replace("\n", "\\n").replace("\"", "\\\""), "\",\"metadata\":\"{\\\"type\\\":\\\"blank\\\"}\", \"added\":\"", datetime.utcfromtimestamp(parsed_data['createdTimestampUsec']/1000000).strftime('%Y-%m-%dT%H:%M:%S.000Z'), "\",\"modified\":\"", datetime.utcfromtimestamp(parsed_data['userEditedTimestampUsec']/1000000).strftime('%Y-%m-%dT%H:%M:%S.000Z'),"\",\"status\":", str(notestatus), ",\"pinned\":", str(notepinned), "}"]) #to anyone modifying this: good luck!
+#structure is "id": {"type": 0,"title": "Title","content": "insert note here","metadata": "{\"type\":\"blank\"}","added": "2024-08-23T07:29:59.161Z","modified": "2024-08-23T07:30:17.821Z","status": 0,"pinned": 1}
+#					checklist or not																				dateTtime.000Z														1-archived 2-trash   2-pinned
+		if id==1:
+			pass
+			file_all.writelines(note_line)
+		else:
+			pass
+			file_all.writelines(',')
+			file_all.writelines(note_line)
+		readout.readlines()
+		filein.close()
+		id=id+1
+	file_all.write('}}')
+	file_all.close()
 if __name__ == '__main__':
-    main()
+	main()
